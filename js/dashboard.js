ed33dea52bae77f16af1d38f40b6de71ac337098
RichardCroz <100240123+Richard-Crozara@users.noreply.github.com>
2026-08-17 16:35:33 -0300

    update

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,11 +1,14 @@
 const token = localStorage.getItem("newworld_token");
 
+const loggedUser = document.getElementById("logged-user");
+const logoutButton = document.getElementById("logout-button");
+
 if (!token) {
     window.location.href = "login.html";
 } else {
     fetch("http://localhost:3000/api/me", {
         headers: {
-            "Authorization": `Bearer ${token}`
+            Authorization: `Bearer ${token}`
         }
     })
         .then(async (response) => {
@@ -17,9 +20,20 @@ if (!token) {
 
             const data = await response.json();
 
-            console.log("Usuário autenticado:", data.user);
+            loggedUser.textContent = `Conectado como: ${data.user.username}`;
         })
         .catch((error) => {
             console.error("Erro ao verificar autenticação:", error);
+
+            localStorage.removeItem("newworld_token");
+            window.location.href = "login.html";
         });
 }
+
+logoutButton.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    localStorage.removeItem("newworld_token");
+
+    window.location.href = "login.html";
+});
